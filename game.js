fbaff45b02788266aa9d17d4db911d5d7537c5d4
App Academy Student <student@appacademy.io>
2016-12-15 18:06:44 -0800

    Implemented towers of hanoi console game

--- game.js
+++ game.js
@@ -10,39 +10,73 @@ class Game {
     this.stacks = [[1,2,3], [], []];
   }
 
+  promptMove (callback) {
+    reader.question('Your move:', res => {
+      const from = parseInt(res[0]);
+      const to = parseInt(res[2]);
+      callback.call(this, from, to);
+
+    });
+  }
+
+  isValidMove (startTowerIdx, endTowerIdx) {
+    if (this.stacks[startTowerIdx].length === 0) {
+      return false;
+    } else if (this.stacks[startTowerIdx].slice(-1)[0] > this.stacks[endTowerIdx].slice(-1)[0]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  move (startTowerIdx, endTowerIdx) {
+    // console.log(startTowerIdx);
+    // console.log(endTowerIdx);
+    // console.log(this);
+    // console.log(this.isValidMove(startTowerIdx, endTowerIdx));
+    if (this.isValidMove(startTowerIdx, endTowerIdx) === true) {
+      let change = this.stacks[startTowerIdx].shift();
+      this.stacks[endTowerIdx].push(change);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  print () {
+    console.log(JSON.stringify(this.stacks));
+  }
+
+  isWon () {
+    if (this.stacks[0] !== []) {
+      return false;
+    } else if (this.stacks[1].length === 3 || this.stacks[2].length === 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  run (completionCallback) {
+    this.print();
+    this.promptMove((from, to) => {
+      this.move(from, to);
+      if (this.isWon() === false) {
+        this.run(completionCallback);
+      } else {
+        console.log('you win!');
+        reader.close();
+        completionCallback();
+      }
+    });
+
+  }
+
 }
 
-Game.prototype.promptMove = function () {
-  console.log(this.stacks);
 
-  reader.question('Your move:', res => {
-    const from = parseInt(res[0]);
-    const to = parseInt(res[2]);
-    return [from, to]
-
-    reader.close();
-  });
-};
-
-Game.prototype.isValidMove = function (startTowerIdx, endTowerIdx) {
-  if (this.stacks[startTowerIdx].length === 0) {
-    return false;
-  } else if (this.stacks[startTowerIdx][this.stacks.length - 1] > this.stacks[endTowerIdx][this.stacks.length - 1]) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
-Game.prototype.move = function (startTowerIdx, endTowerIdx) {
-  if (this.isValidMove(startTowerIdx, endTowerIdx)) {
-    let change = this.stacks[startTowerIdx].shift();
-    this.stacks[endTowerIdx].push(change);
-    console.log(this.stacks);
-    return true;
-  } else {
-    return false;
-  }
-};
 const a = new Game();
-a.move(0,1);
+
+a.run(function(){
+  console.log('You won!');
+});
